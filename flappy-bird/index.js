b00f3267727398efe77e0de60dc2a0c9b819ee11
王龙祥 <longxiang.wang@vipshop.com>
2017-05-09 17:39:17 +0800

    鸟能够飞，并添加背景

--- flappy-bird/index.js
+++ flappy-bird/index.js
@@ -1,22 +1,9 @@
-class Component {
-    constructor(x, y, width, height) {
+class Obstacle {
+    constructor(context, x, y, width, height, speedX = 1, color = 'green') {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-    }
-
-    ifCrashWith(component) {
-        if ((this.x > component.x + component.width) || (this.x + this.width < component.x) || (this.y > component.y + component.height) || (this.y + this.height < component.y)){
-            return false;
-        }
-        return true;
-    }
-}
-
-class Obstacle extends Component {
-    constructor(context, x, y, width, height, speedX = 1, color = 'green') {
-        super(x, y, width, height);
         this.context = context;
         this.speedX = speedX;
         this.color = color;
@@ -32,41 +19,86 @@ class Obstacle extends Component {
     }
 }
 
-class Bird extends Component {
-    constructor(context, x, y, width, height) {
-        super(x, y, width, height);
+class Bird {
+    constructor(context, x, y, width, height, flyV = -10, a = 0.3) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.fly = this.fly.bind(this);
+
         this.context = context;
-        this.color = 'red';
-        this.a = 10;
+        this.image = document.querySelector('#bird');
+        this.yCeil = this.context.canvas.height - this.height / 2;
+        this.yFloor = this.height / 2;
+        // this.flyHeight = 30;
+        this.v = 0;
+        this.flyV = flyV;
+        this.a = a;
+        // this.flyVelocity = -Math.sqrt(2 * this.a * this.flyHeight);
+        this.frameCounter = -1;
+        this.sy = 0;
     }
 
     updatePosition() {
+        this.frameCounter = (this.frameCounter + 1) % 18;
+        this.sy = Math.floor(this.frameCounter / 18 * 3);
+        this.v += this.a;
+        this.y += this.v;
+        if (this.y < this.yFloor) {
+            this.y = this.yFloor;
+            this.v = 0;
+        } else if (this.y > this.yCeil) {
+            this.y = this.yCeil;
+            this.v = 0;
+        }
         let context = this.context;
         context.save();
-        context.fillStyle = this.color;
-        context.fillRect(this.x, this.y, this.width, this.height);
+        context.translate(this.x, this.y);
+        // context.fillStyle = 'grey';      // debug
+        // context.fillRect(0, -this.height / 2, this.width, this.height);   // debug
+        context.drawImage(this.image, 0, this.sy * 60, 88, 60, 0, -this.height / 2, this.width, this.height);
         context.restore();
+    }
+
+    fly() {
+        this.v = -7;
+    }
+
+    ifCrashWith(obstacle) {
+        if ((this.x > obstacle.x + obstacle.width) || (this.x + this.width < obstacle.x) || (this.y - this.height / 2 > obstacle.y + obstacle.height) || (this.y + this.height / 2 < obstacle.y)){
+            return false;
+        }
+        console.log(this);  // debug
+        console.log(obstacle); // debug
+        return true;
     }
 }
 
 class Game {
-    constructor() {
+    constructor({bgImage}) {
         this.refresh = this.refresh.bind(this);
+        this.fly = this.fly.bind(this);
+
         this.canvas = document.createElement('canvas');
         this.width = this.canvas.width = 400;
-        this.height = this.canvas.height = 247;
+        this.height = this.canvas.height = 650;
         this.context = this.canvas.getContext('2d');
         this.obstMinHeight = this.height * 0.2;
         this.obstHeightDelta = this.height * 0.5;
         this.gapMinHeight = this.height * 0.2;
         this.gapHeightDelta = this.height * 0.1;
 
+        this.bgImage = bgImage;
         this.obstacles = [];
-        this.lastObstTime = Date.now();
-        this.bird = new Bird(this.context, 10, (this.height - 10) / 2, 10, 10);
+        this.obstTimeInterval = 4000;
+        this.lastObstTime = Date.now() - this.obstTimeInterval;
+        this.bird = new Bird(this.context, 10, (this.height - 10) / 2, 44, 30);
         this.crashed = false;
 
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+
+        this.listenFly();
     }
 
     addTwoObstacles() {
@@ -75,15 +107,24 @@ class Game {
             gapHeight = this.gapMinHeight + Math.random() * this.gapHeightDelta,
             obstBottomY = obstTopHeight + gapHeight,
             obstBottomHeight = this.height - obstBottomY;
-        this.obstacles.push(new Obstacle(this.context, this.width, 0, 20, obstTopHeight));
-        this.obstacles.push(new Obstacle(this.context, this.width, obstBottomY, 20, obstBottomHeight));
+        this.obstacles.push(new Obstacle(this.context, this.width, 0, 50, obstTopHeight));
+        this.obstacles.push(new Obstacle(this.context, this.width, obstBottomY, 50, obstBottomHeight));
     }
 
     clear() {
         this.context.clearRect(0, 0, this.width, this.height);
     }
 
-    getNewFrame() {
+    fillBackground() {
+        let context = this.context;
+        context.save();
+        context.drawImage(this.bgImage, 0, 0, this.width, this.height);
+        context.restore();
+    }
+
+    frame() {
+        this.fillBackground();
+
         // 清除已经移出左边界的障碍物
         while (true) {
             if (this.obstacles.length === 0) {
@@ -99,7 +140,7 @@ class Game {
 
         // 新增障碍物
         let now = Date.now();
-        if (now - this.lastObstTime > 2000) {
+        if (now - this.lastObstTime > this.obstTimeInterval) {
             this.addTwoObstacles();
             // console.log(this.obstacles.length);  // debug
         }
@@ -124,12 +165,17 @@ class Game {
                 break;
             }
         }
+
+        if (this.bird.y + this.bird.height / 2 >= this.height) {
+            this.crashed = true;
+        }
+
         return this.crashed;
     }
 
     refresh() {
         this.clear();
-        this.getNewFrame();
+        this.frame();
         if (this.checkCrash()) {
             this.stop();
         } else {
@@ -144,11 +190,26 @@ class Game {
 
     stop() {
         window.cancelAnimationFrame(this.nextFrame);
+        this.ignoreFly();
         console.log('stop');
+    }
+
+    fly() {
+        this.bird.fly();
+    }
+
+    listenFly() {
+        this.canvas.addEventListener('click', this.fly, false);
+    }
+
+    ignoreFly() {
+        this.canvas.removeEventListener('click', this.fly, false);
     }
 }
 
-let game = new Game();
+let game = new Game({
+    bgImage: document.querySelector('#bg')
+});
 game.start();
 /*setTimeout(()=>{
     game.stop();
