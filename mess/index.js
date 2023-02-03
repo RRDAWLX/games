75a0d22cc2b1aab48dbf113e5c618d7d6f8a221d
rrdawlx <815311850@qq.com>
2023-02-03 16:26:19 +0800

    add progress bar

--- mess/index.js
+++ mess/index.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas')
 const width = canvas.width = document.body.clientWidth
-const height = canvas.height = document.body.clientHeight
+const height = canvas.height = document.body.clientHeight - 16
 const ctx = canvas.getContext('2d')
 const radius = 3 // 圆点半径，单位 px
 
@@ -28,11 +28,32 @@ function drawLine(x1, y1, x2, y2) {
 let prePoint = [Math.floor(width / 2), Math.floor(height / 2)]
 drawCircle(...prePoint)
 
+// 随机绘画
 function randomDraw() {
   const [x, y] = getRandomPoint()
   drawLine(prePoint[0], prePoint[1], x, y)
   drawCircle(x, y)
   prePoint = [x, y]
+}
+
+// 进度条相关功能
+const rateDom = document.getElementById('rate')
+const percentageDom = document.getElementById('percentage')
+
+function calcDrawingPercentage() {
+  const { data } = ctx.getImageData(0, 0, width, height)
+  let count = 0
+
+  for (let i = 3; i < data.length; i += 4) {
+    if (data[i]) {
+      count++
+    }
+  }
+
+  const percentage = count * 4 / (width * height)
+  rateDom.style.cssText = `width: ${percentage}%`
+  percentageDom.innerText = percentage.toFixed(4) + ' %'
+  return percentage
 }
 
 let task = null
@@ -42,6 +63,13 @@ canvas.addEventListener('click', function() {
     clearInterval(task)
     task = null
   } else {
-    task = setInterval(randomDraw, 100)
+    task = setInterval(() => {
+      randomDraw()
+
+      if (calcDrawingPercentage() === 100) {
+        clearInterval(task)
+        task = null
+      }
+    }, 100)
   }
 })
